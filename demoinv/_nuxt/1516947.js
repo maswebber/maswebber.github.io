3513df9968faf7c9f93d87ce41c8101ca6acbb33
maswebber <mas.webber01@gmail.com>
2023-01-15 19:27:00 +0700

    Prototype Added

---
+++ demoinv/_nuxt/1516947.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{310:function(n,t,e){"use strict";e.r(t);var l={name:"MiddleSection"},o=e(34),component=Object(o.a)(l,(function(){return(0,this._self._c)("h1",{staticClass:"text-3xl font-bold underline"},[this._v("\n  Middle\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
